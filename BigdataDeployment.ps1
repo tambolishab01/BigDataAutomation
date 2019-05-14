34983a387a026498eb56657c196bea308f67d975
alankarmeha <alankar.mehta@globant.com>
2019-05-14 12:43:00 +0530

    commit2

---
+++ BigdataDeployment.ps1
@@ -0,0 +1,3 @@
+﻿Login-AzureRmAccount
+Select-AzureRmSubscription -Subscription "07494aa9-3134-4cdd-9e14-e0d21257696c"
+New-AzureRmResourceGroupDeployment -Name bigdatadeployment -ResourceGroupName bigdatademoRG -TemplateUri "https://raw.githubusercontent.com/tambolishab01/BigData/master/Main_Template1.json"
